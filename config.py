196881404bfeadd95d6674f0d695f5f9806781fa
779943132 <62226914+779943132@users.noreply.github.com>
2024-07-11 08:20:41 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,26 @@
+import os
+
 # 连接端口
-PORT = 8443
+PORT = os.environ.get("pord", 2024)
 
 # 密钥（32个十六进制字符）
 USERS = {
-    "tg":  "bc6dddb180509c5b18cb51d17d849455",
-    # "tg2": "fa33e8ed3523ce760c241129c66153b6",
+    "tg":  os.environ.get("users","9bb2d2306dfeb32ee7fb549acf0f16c6")
 }
 
 MODES = {
     # 经典模式，容易被墙
-    "classic": False,
+    "classic": os.environ.get("classic",False),
 
     # 混淆伪装
-    "secure": True,
+    "secure": os.environ.get("secure",True),
 
     # TLS加密
-    "tls": True
+    "tls": os.environ.get("tls",True)
 }
 
 # 伪装域名
-TLS_DOMAIN = "www.swift.com"
+TLS_DOMAIN = os.environ.get("domain","www.apple.com")
 
 # 广告标签，可从 @MTProxybot 获取
 # AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
-
